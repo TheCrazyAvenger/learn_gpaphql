9384b86604669c7d650ec31b0305f872993781b4
Illia Puliushchyk <ilyapavlushchik221@gmail.com>
2022-01-12 16:22:35 +0300

    Completed first challenge

--- src/index.js
+++ src/index.js
@@ -3,12 +3,42 @@ const {GraphQLServer} = require('graphql-yoga');
 const typeDefs = `
 type Query {
     helloWorld: String!
+    users: [User!]!
+}
+
+type User {
+    id: ID!
+    firstName: String!
+    email: String!
+    age: Int!
 }
 `;
+
+let users = [
+  {
+    id: '123',
+    firstName: 'Ilya',
+    email: 'ilya@mail.ru',
+    age: 32,
+  },
+  {
+    id: '456',
+    firstName: 'Dan',
+    email: 'dan@mail.ru',
+    age: 19,
+  },
+];
 
 const resolvers = {
   Query: {
     helloWorld: () => `Hello World!`,
+    users: (parent, args, context, info) => users,
+  },
+  User: {
+    id: parent => parent.id,
+    firstName: parent => parent.firstName,
+    email: parent => parent.email,
+    age: parent => Math.round(Math.random() * 4 + 1) * parent.age,
   },
 };
 
